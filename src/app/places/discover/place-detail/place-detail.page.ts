e0cb511efb6e88e7726f9e971d2bd97a9dbaf2d6
Cheng Wang <cheng@Chengs-MacBook-Pro.local>
2022-03-17 10:33:32 -0400

    ability to log bookings

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model';
+import { CreateBookingComponent} from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,11 +11,37 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
-  constructor(private router: Router, private navCtrl: NavController) {}
+  place: Place;
+  constructor(private route: ActivatedRoute, 
+    private navCtrl: NavController,
+    private placesService: PlacesService,
+    private modalCtrl: ModalController
+    ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has ('placeId')) {
+      this.navCtrl.navigateBack('/places/tabs/discover');
+      return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      });
+  }
 
   onBookPlace() {
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    this.modalCtrl.create({
+      component: CreateBookingComponent,
+      componentProps: {selectedPlace: this.place}
+    })
+    .then(modalEl => { 
+      modalEl.present(); 
+      return modalEl.onDidDismiss();
+    })
+    .then(resultData => {
+      console.log(resultData.data, resultData.role);
+      if(resultData.role === 'confirm') {
+        console.log('BOOKED!')
+      }
+    });
   }
 }
